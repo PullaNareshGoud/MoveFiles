58ffe1344674869bd1f716b83f824e403a9d237f
PULLA NARESH GOUD <pullanareshgoud@gmail.com>
2021-07-17 15:17:40 +0530

    Working code with SMB

--- src/main/java/com/fileutil/MoveFile/SecureTransferFiles.java
+++ src/main/java/com/fileutil/MoveFile/SecureTransferFiles.java
@@ -1,31 +1,44 @@
 package com.fileutil.MoveFile;
 
 import jcifs.smb.NtlmPasswordAuthentication;
-import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 
-import java.net.MalformedURLException;
-
 public class SecureTransferFiles {
-    //source (file://LAPTOP-NCVRG7VT/source)
-    public static void main(String[] args) throws MalformedURLException, SmbException {
-//        String sourcePath = "smb://192.168.16.1/source/";
-        String sourcePath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Pending";
-        String destPath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Processing";
-//        String destPath = "smb://192.168.16.1/dest/";
+    public void moveFilesFromPendingToProcessing() {
+        String sourcePath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Pending/";
+        String destPath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Processing/";
+        moveFilesFromSourceToDest(sourcePath, destPath);
+    }
 
-        System.out.println("source: " + sourcePath);
-        System.out.println("destination: " + destPath);
+    public void moveFilesFromProcessingToComplete() {
+        String sourcePath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Processing/";
+        String destPath = "smb://OTCFlatFile-NonProd.bp.com/OTCFlatFile-NonProd$/IL_Batch/ENDUR/Complete/";
 
-        NtlmPasswordAuthentication sourceAuth = new NtlmPasswordAuthentication(null, "-svc-ist-xtx-cntrcti", "JO$3WNA7&Lmg1!");
-        NtlmPasswordAuthentication destAuth = new NtlmPasswordAuthentication(null, "-svc-ist-xtx-cntrcti", "JO$3WNA7&Lmg1!");
-        System.out.println("transferring files..");
-        SmbFile dir = new SmbFile(sourcePath, sourceAuth);
-        for (SmbFile f : dir.listFiles()) {
-            System.out.println("transferring file " + f.getName());
-            SmbFile destFile = new SmbFile(destPath + f.getName(), destAuth);
-            f.copyTo(destFile);
-            f.delete();
+        moveFilesFromSourceToDest(sourcePath, destPath);
+    }
+
+    public void moveFilesFromSourceToDest(String sourcePath, String destPath) {
+        try {
+
+            NtlmPasswordAuthentication sourceAuth = new NtlmPasswordAuthentication(null, "username", "pwd");
+            SmbFile file = new SmbFile(sourcePath, sourceAuth);
+            System.out.println("source directory: " + sourcePath);
+            System.out.println("destination directory: " + destPath);
+            for (SmbFile f : file.listFiles()) {
+                System.out.println("transferring file: " + f);
+                System.out.println("source path: " + f);
+                SmbFile dest = new SmbFile(destPath + f.getName(), sourceAuth);
+                System.out.println("  copy file to destination: " + dest);
+                f.copyTo(dest);
+                System.out.println("  copied file to destination: " + dest);
+                System.out.println("  delete file from source: " + f);
+                f.delete();
+                System.out.println("  deleted file from source: " + f);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
